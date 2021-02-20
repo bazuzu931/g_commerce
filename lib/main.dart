16e3abe926e2f5057c9f9fd55e259684b0597f0f
bazuzu931 <bazuzu931@yandex.ru>
2021-02-20 22:20:55 +0400

    add_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      // debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('title'),
+          centerTitle: true,
+        ),
+      ),
     );
   }
 }
